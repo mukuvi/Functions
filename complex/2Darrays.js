b454e20ff75ee708301ef3ae44c1474c6add699c
jmngandu <jamesmngandu@gmail.com>
2025-02-02 14:07:37 +0300

    functions

--- complex/2Darrays.js
+++ complex/2Darrays.js
@@ -1,19 +1,8 @@
 // 2 D arrays
-let numberGrid = [
-    [
-        1, 2, 3
-    ],
-    [
-        4, 5, 6
-    ],
-    [
-        7, 8, 9
-    ],
-    [0]
-]
+let numberGrid = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]];
 for (let i = 0; i < numberGrid.length; i++) {
-    for (let j = 0; j < numberGrid[i].length; j++) {
-        console.log(numberGrid[i][j]);
-    }
-    console.log("<br/>");
+  for (let j = 0; j < numberGrid[i].length; j++) {
+    console.log(numberGrid[i][j]);
+  }
+  console.log("<br/>");
 }
