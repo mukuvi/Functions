5ccc57726fe5fe29ed3ccc9ee63dc28c6b3e3c51
jmngandu <jamesmngandu@gmail.com>
2025-02-02 10:52:40 +0300

    commit

--- maps/capitalized.js
+++ maps/capitalized.js
@@ -3,11 +3,16 @@ Given an array of strings, return an array where
 the first letter of each string is capitalized
 */
 
-const names = ['alice', 'bob', 'charlie', 'danielle'];
+const names = ["alice", "bob", "charlie", "danielle"];
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
 const capitalized = names.map((name) => {
-	return name[0].toUpperCase() + name.slice(1);
+  return name[0].toUpperCase() + name.slice(1);
 });
 
 console.log(capitalized);
+
+const lowercase = names.map((nam) => {
+  return nam[0].toUpperCase() + nam.slice(1);
+});
+console.log(lowercase);
