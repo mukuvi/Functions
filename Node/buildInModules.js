9368ffbef985ba0d0dc33aea29da9e68ef4dc05f
jmngandu <jamesmngandu@gmail.com>
2025-02-10 23:24:22 +0300

    functions

--- Node/buildInModules.js
+++ Node/buildInModules.js
@@ -1,11 +1,12 @@
 const path = require("node:path");
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-console.log(path.extname(__filename));
-console.log(path.extname(__dirname));
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
 
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
+console.log(path.join("folder1", "folder2", "index.js"));
