c1b617d3246c07f14dce717528586d21417ab9a2
jmngandu <jamesmngandu@gmail.com>
2025-02-11 22:45:32 +0300

    commit

--- node-Crash/server.js
+++ node-Crash/server.js
@@ -1,6 +1,10 @@
 import http from "http";
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
   res.write("hello world");
   res.end();
+});
+server.listen(8000, () => {
+  console.log(`server running on port ${PORT}`);
 });
