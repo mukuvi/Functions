aa861a856816d5914268c83aa8513ec63ae25f74
jmngandu <jamesmngandu@gmail.com>
2025-02-12 13:17:06 +0300

    functions

---
+++ node-Crash/osDemo.js
@@ -0,0 +1,10 @@
+import os from "os";
+
+//useInfo()
+console.log(os.userInfo());
+//totalMem()
+console.log(os.totalmem());
+//freeMem()
+console.log(os.freemem());
+//cpus()
+console.log(os.cpus());
