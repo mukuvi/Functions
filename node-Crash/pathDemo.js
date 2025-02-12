3c112c9683abfb05aa1055a2dc2f1475f3f884e9
jmngandu <jamesmngandu@gmail.com>
2025-02-12 12:59:27 +0300

    functions

---
+++ node-Crash/pathDemo.js
@@ -0,0 +1,12 @@
+import path from "path";
+
+const filePath = "./dir1/dir2/test.txt";
+
+//basename
+console.log(path.basename(filePath));
+
+//dirname
+console.log(path.dirname(filePath));
+
+//extname
+console.log(path.extname(filePath));
