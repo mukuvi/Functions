c6176457ce22e94622eed4bd6fca11e958d4acd5
jmngandu <jamesmngandu@gmail.com>
2025-02-12 13:45:19 +0300

    functions

--- node-Crash/cryptoDemo.js
+++ node-Crash/cryptoDemo.js
@@ -15,3 +15,18 @@ crypto.randomBytes(16, (err, buf) => {
 const algorithm = "aes-256-cbc";
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
+
+//encrypt
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update(
+  "hello, this is is a secret message",
+  "utf8",
+  "hex"
+);
+encrypted += cipher.final("hex");
+console.log(encrypted);
+//decrypt
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, "hex", "utf8");
+decrypted += decipher.final("utf8");
+console.log(decrypted);
