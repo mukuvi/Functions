c7443872ba77f9c7d34f896d48fa21f01f26795e
jmngandu <jamesmngandu@gmail.com>
2025-02-12 17:25:06 +0300

    functions

--- Express/server.js
+++ Express/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const path = require("path");
-
+const port = process.env.PORT || 8000;
 const app = express();
 
 //setup static folder
-app.use(express.static(path.join(__dirname, "public")));
-const PORT = 8000;
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+
+//Get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
+
+//Get single posts
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
