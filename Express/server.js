b914dcf7a2c76ed1a0b4a208095c02483ea49b14
jmngandu <jamesmngandu@gmail.com>
2025-02-12 16:26:50 +0300

    functions

--- Express/server.js
+++ Express/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3030;
+const PORT = 8000;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running under port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
