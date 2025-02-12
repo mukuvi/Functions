3ef607fe61293fbeafcf8e534f271128c3777e2a
jmngandu <jamesmngandu@gmail.com>
2025-02-12 10:12:23 +0300

    functions

--- node-Crash/fsDemo.js
+++ node-Crash/fsDemo.js
@@ -1,1 +1,10 @@
+import { error } from "console";
 import fs from "fs";
+
+//readFile - callback
+fs.readFile("./test.txt", "utf-8", (error, data) => {
+  if (error) throw error;
+  console.log(data);
+});
+
+//readFileSync -synchronous
