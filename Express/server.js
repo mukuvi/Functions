1c21dbca74cb61740b2b261271f382d457aa6cb9
jmngandu <jamesmngandu@gmail.com>
2025-02-12 16:44:10 +0300

    functions

--- Express/server.js
+++ Express/server.js
@@ -3,14 +3,10 @@ const path = require("path");
 
 const app = express();
 
+//setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 const PORT = 8000;
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
