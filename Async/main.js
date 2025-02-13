d1ab3e816b40661e702d7d29a547a64ff2478949
jmngandu <jamesmngandu@gmail.com>
2025-02-13 08:34:59 +0300

    functions

---
+++ Async/main.js
@@ -0,0 +1,11 @@
+let stocks = {
+  fruits: ["strawberry", "grapes", "bananas", "mamgo"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+let is_shop_open = true;
+let order = (time, work) => {
+  return new Promise();
+};
