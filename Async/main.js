978e5a761c6a2ef5962d17bebfab6fb9b61d681d
jmngandu <jamesmngandu@gmail.com>
2025-02-13 08:42:02 +0300

    functions

--- Async/main.js
+++ Async/main.js
@@ -7,5 +7,14 @@ let stocks = {
 
 let is_shop_open = true;
 let order = (time, work) => {
-  return new Promise();
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("The shop is closed"));
+    }
+  });
 };
+order(2000, () => console.log(`${stocks.fruits[0]}`));
